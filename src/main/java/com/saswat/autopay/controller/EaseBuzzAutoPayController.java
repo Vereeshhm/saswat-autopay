70e1626d8f154304995a89ba9c77d9b9c30a79e5
anuj.verma@saswatfinance.com <anuj.verma@saswatfinance.com>
2024-09-12 18:42:28 +0530

    Made all field as params change

--- src/main/java/com/saswat/autopay/controller/EaseBuzzAutoPayController.java
+++ src/main/java/com/saswat/autopay/controller/EaseBuzzAutoPayController.java
@@ -6,12 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.saswat.autopay.dto.CancelMandateDto;
@@ -21,7 +20,7 @@ import com.saswat.autopay.dto.RegisterAutopayRequestDto;
 import com.saswat.autopay.service.EasebuzzAutopayRegisterService;
 import com.saswat.autopay.service.EasebuzzEnachRegisterService;
 
-@RestController
+@Controller
 @RequestMapping("/saswat")
 public class EaseBuzzAutoPayController {
 	
@@ -33,46 +32,70 @@ public class EaseBuzzAutoPayController {
 	@Autowired
 	EasebuzzEnachRegisterService easebuzzEnachRegisterService;
 
+	/*
+	 * @PostMapping("/v1/payment/initiate") public ResponseEntity<String>
+	 * InitiatePay(@RequestParam("amount") Float amount,
+	 * 
+	 * @RequestParam("productinfo") String productinfo,
+	 * 
+	 * @RequestParam("firstname") String firstname,
+	 * 
+	 * @RequestParam("phone") String phone,
+	 * 
+	 * @RequestParam("email") String email,
+	 * 
+	 * @RequestParam("udf1") String udf1,
+	 * 
+	 * @RequestParam("maxAmount") Float maxAmount,
+	 * 
+	 * @RequestParam("address1") String address1,
+	 * 
+	 * @RequestParam("address2") String address2,
+	 * 
+	 * @RequestParam("city") String city,
+	 * 
+	 * @RequestParam("state") String state,
+	 * 
+	 * @RequestParam("country") String country,
+	 * 
+	 * @RequestParam("zipcode") String zipcode,
+	 * 
+	 * @RequestParam("final_collection_date") String finalCollectionDate,
+	 * 
+	 * @RequestParam("show_payment_mode") String showPaymentMode) throws Exception {
+	 * 
+	 * 
+	 * RegisterAutopayRequestDto registerAutopayRequestDto = new
+	 * RegisterAutopayRequestDto(); registerAutopayRequestDto.setAmount(amount);
+	 * registerAutopayRequestDto.setProductinfo(productinfo);
+	 * registerAutopayRequestDto.setFirstname(firstname);
+	 * registerAutopayRequestDto.setPhone(phone);
+	 * registerAutopayRequestDto.setEmail(email);
+	 * registerAutopayRequestDto.setUdf1(udf1);
+	 * registerAutopayRequestDto.setMaxAmount(maxAmount);
+	 * registerAutopayRequestDto.setAddress1(address1);
+	 * registerAutopayRequestDto.setAddress2(address2);
+	 * registerAutopayRequestDto.setCity(city);
+	 * registerAutopayRequestDto.setState(state);
+	 * registerAutopayRequestDto.setCountry(country);
+	 * registerAutopayRequestDto.setZipcode(zipcode);
+	 * registerAutopayRequestDto.setFinal_collection_date(finalCollectionDate);
+	 * registerAutopayRequestDto.setShow_payment_mode(showPaymentMode);
+	 * 
+	 * logger.info("Autopay Register Request : ",registerAutopayRequestDto);
+	 * 
+	 * return easebuzzautopayservice.registerAutopay(registerAutopayRequestDto); }
+	 */
+	
+	
 	@PostMapping("/v1/payment/initiate")
-	public ResponseEntity<String> InitiatePay(@RequestParam("amount") Float amount,
-		    @RequestParam("productinfo") String productinfo,
-		    @RequestParam("firstname") String firstname,
-		    @RequestParam("phone") String phone,
-		    @RequestParam("email") String email,
-		    @RequestParam("udf1") String udf1,
-		    @RequestParam("maxAmount") Float maxAmount,
-		    @RequestParam("address1") String address1,
-		    @RequestParam("address2") String address2,
-		    @RequestParam("city") String city,
-		    @RequestParam("state") String state,
-		    @RequestParam("country") String country,
-		    @RequestParam("zipcode") String zipcode,
-		    @RequestParam("final_collection_date") String finalCollectionDate,
-		    @RequestParam("show_payment_mode") String showPaymentMode)
-			throws Exception {
-		
-		
-		RegisterAutopayRequestDto registerAutopayRequestDto = new RegisterAutopayRequestDto();
-	    registerAutopayRequestDto.setAmount(amount);
-	    registerAutopayRequestDto.setProductinfo(productinfo);
-	    registerAutopayRequestDto.setFirstname(firstname);
-	    registerAutopayRequestDto.setPhone(phone);
-	    registerAutopayRequestDto.setEmail(email);
-	    registerAutopayRequestDto.setUdf1(udf1);
-	    registerAutopayRequestDto.setMaxAmount(maxAmount);
-	    registerAutopayRequestDto.setAddress1(address1);
-	    registerAutopayRequestDto.setAddress2(address2);
-	    registerAutopayRequestDto.setCity(city);
-	    registerAutopayRequestDto.setState(state);
-	    registerAutopayRequestDto.setCountry(country);
-	    registerAutopayRequestDto.setZipcode(zipcode);
-	    registerAutopayRequestDto.setFinal_collection_date(finalCollectionDate);
-	    registerAutopayRequestDto.setShow_payment_mode(showPaymentMode);
-		
-		logger.info("Autopay Register Request : ",registerAutopayRequestDto);
+	public ResponseEntity<String> InitiatePay(@RequestBody RegisterAutopayRequestDto registerAutopayRequestDto) throws Exception {
+				
+		logger.info("Autopay Register Request : " + registerAutopayRequestDto.toString());
 
 		return easebuzzautopayservice.registerAutopay(registerAutopayRequestDto);
 	}
+	
 
 	@PostMapping("/v1/payment/debitRequest")
 	public ResponseEntity<String> DebitRequest(@ModelAttribute DebitAutopayRequestDto debitAutopayRequestDto)
